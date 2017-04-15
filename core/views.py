982fb78e73cc98b9f0e2828cde025a790ad27f81
Lucas Felix <lucasfelix225@hotmail.com>
2017-04-15 18:50:08 -0300

    Adição de outros templates e modificação em core/views.py e djangoecommerce/urls.py

--- core/views.py
+++ core/views.py
@@ -10,3 +10,15 @@ objeto response
 
 def index(request):
     return render(request, 'index.html')
+
+
+def contact(request):
+    return render(request, 'contact.html')
+
+
+def product_list(request):
+    return render(request, 'product_list.html')
+
+
+def product(request):
+    return render(request, 'product.html')

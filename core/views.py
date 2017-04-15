8b9d99e10d52f4726b850e3efb4137c0975270b4
Lucas Felix <lucasfelix225@hotmail.com>
2017-04-15 17:23:30 -0300

    Adição de app 'core' e configuração em setings.py e modificação em urls.py

---
+++ core/views.py
@@ -0,0 +1,12 @@
+# coding=utf-8
+from django.shortcuts import render
+from django.http import HttpResponse
+
+"""
+View é uma função Python que recebe um objeto requests e tem que retornar um
+objeto response
+"""
+
+
+def index(request):
+    return HttpResponse('Hello world!')

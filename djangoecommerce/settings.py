e6044f74ed8f245c3b731944817323c8c052cdd5
Lucas Felix <lucasfelix225@hotmail.com>
2017-04-18 00:27:19 -0300

    Alteração em djangoecommerce/settings.py e adição de três arquivos, sendo eles, Procfile, requirements.txt e runtime.txt

--- djangoecommerce/settings.py
+++ djangoecommerce/settings.py
@@ -13,7 +13,8 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))  # raiz
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))  # relativo settings
 
 
 # Quick-start development settings - unsuitable for production
@@ -119,3 +120,20 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# Update database configuration with $DATABSE_URL
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = [*]
+
+# Static files (CSS, Javascript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')  #

5cb15ff3a03b047e38e22c9e5394ed31000ba9fd
Lucas Felix <lucasfelix225@hotmail.com>
2017-04-18 01:18:23 -0300

    Correção em settings.py

--- djangoecommerce/settings.py
+++ djangoecommerce/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))  # raiz
@@ -123,6 +124,7 @@ STATIC_URL = '/static/'
 
 
 # Update database configuration with $DATABSE_URL
+db_from_env = dj_database_url.config(conn_max_age=500)
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 

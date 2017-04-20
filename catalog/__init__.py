78d32370347fdc6c85554cc5e5c5a45034a9662e
Lucas Felix <lucasfelix225@hotmail.com>
2017-04-20 00:49:25 -0300

    Adição de duas classes em catalog/admin.py e alterações em catalog/__init__.py e catalog/apps.py

---
+++ catalog/__init__.py
@@ -0,0 +1,4 @@
+# coding=utf-8
+
+default_app_config = 'catalog.apps.CatalogConfig'
+# altera o nome padrão da app, passando de 'catalog' para 'Catálogo'
--- catalog/admin.py
+++ catalog/admin.py
@@ -4,5 +4,20 @@ from django.contrib import admin
 
 from .models import Category, Product
 
-admin.site.register(Category)
-admin.site.register(Product)
+
+# CategoryAdmin é uma classe que vai herdar de modelAdmin
+class CategoryAdmin(admin.ModelAdmin):
+    # lista de campos que serão exibidos na listagem
+    list_display = ['name', 'slug', 'created_at', 'modified']
+    search_fields = ['name', 'slug']  # opções de buscas
+    list_filter = ['created_at', 'modified']
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug', 'category', 'created_at', 'modified']
+    search_fields = ['name', 'slug', 'category__name']  # opções de buscas
+    list_filter = ['created_at', 'modified']  # filtro lateral
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Product, ProductAdmin)

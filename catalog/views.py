750d80605daedd61051a10e0ed4c1d2fa8022e04
Lucas Felix <lucasfelix225@hotmail.com>
2017-04-20 01:45:40 -0300

    Alterações em catalog/views.py, base.html, core/views.py e urls.py. E adição de dois arquivos, templates/catalog/product_list.html e catalog/urls.py

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+# coding=utf-8
 
-# Create your views here.
+from django.shortcuts import render
+from .models import Category, Product
+
+
+def product_list(request):
+    context = {
+        'product_list': Product.objects.all()
+    }
+    return render(request, 'catalog/product_list.html', context)

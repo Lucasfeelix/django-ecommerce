7e7232c1ec9769d955dbd9ef3f434b07390dfb8e
Lucas Felix <lucasfelix225@hotmail.com>
2017-05-06 12:11:06 -0300

    Aula 40 - Criacao de CBS RegisterView em core/views.py, criacao de template register.html e modificacao em urls.py

--- core/views.py
+++ core/views.py
@@ -2,8 +2,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.core.mail import send_mail
+from django.core.urlresolvers import reverse_lazy
 from django.conf import settings
-from django.views.generic import View, TemplateView
+from django.views.generic import View, TemplateView, CreateView
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import get_user_model
 from catalog.models import Category, Product
 from .forms import ContactForm
 
@@ -11,6 +14,7 @@ from .forms import ContactForm
 View é uma função Python que recebe um objeto requests e tem que retornar um
 objeto response.
 """
+User = get_user_model()
 
 
 class IndexView(TemplateView):
@@ -32,19 +36,30 @@ def contact(request):
     }
     return render(request, 'contact.html', context)
 
-    # sucess = False
-    # if request.method == 'POST':
-    #     form = ContactForm(request.POST)
-    #     if form.is_valid():
-    #         form.send_mail()
-    #         sucess = True
-    # else:
-    #     form = ContactForm()
-    # context = {
-    #     'form': form,
-    #     'sucess': sucess,
-    # }
-    # return render(request, 'contact.html', context)
+
+class RegisterView(CreateView):
+    form_class = UserCreationForm  # indica qual a classe de formulário usada
+    template_name = 'register.html'
+    model = User
+    success_url = reverse_lazy('index')
+    # faz com que após o cadastro o usuário seja redirecionado para o index
+
+
+register = RegisterView.as_view()
+
+# sucess = False
+# if request.method == 'POST':
+#     form = ContactForm(request.POST)
+#     if form.is_valid():
+#         form.send_mail()
+#         sucess = True
+# else:
+#     form = ContactForm()
+# context = {
+#     'form': form,
+#     'sucess': sucess,
+# }
+# return render(request, 'contact.html', context)
 
 # def get(self, request):
 #     return render(request, 'index.html')

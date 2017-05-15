1e33ef9b59e19478aee044d6ee5fb8cfb349230a
Lucas Felix <lucasfelix225@hotmail.com>
2017-05-15 00:51:21 -0300

    Aula 48 - Objetivo: atualização dos teste de login e cadastro; adição de tests em account e remoção te test de registro de usuário de core

--- core/tests/test_views.py
+++ core/tests/test_views.py
@@ -80,21 +80,7 @@ class LoginViewTestCase(TestCase):
         response = self.client.post(self.login_url, data)
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'login.html')
-        error_msg = ('Por favor, entre com um usuário  e senha corretos.'
+        error_msg = ('Por favor, entre com um Username / Usuário  e senha corretos.'
                      ' Note que ambos os campos diferenciam maiúsculas e '
                      'minúsculas.')
         self.assertFormError(response, 'form', None, error_msg)
-
-
-class RegisterViewTestCase(TestCase):
-    def setUp(self):
-        self.client = Client()
-        self.register_url = reverse('register')
-
-    def test_register_ok(self):
-        data = {'username': 'lucas', 'password1': 'teste123',
-                'password2': 'teste123'}
-        response = self.client.post(self.register_url, data)
-        index_url = reverse('index')
-        self.assertRedirects(response, index_url)
-        self.assertEqual(User.objects.count(), 1)

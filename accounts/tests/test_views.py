7113b37048b3a08ecc8ea70032c00aac10afff9a
Lucas Felix <lucasfelix225@hotmail.com>
2017-05-23 00:43:50 -0300

    Aula 51 - Objetivo: Criar testes da app Accounts. Criação de testes em accounts/tests/test_views.py, modificação da class UpdatePasswordView em accounts/views.py e modificação em index.html.

--- accounts/tests/test_views.py
+++ accounts/tests/test_views.py
@@ -1,6 +1,8 @@
 from django.test import Client, TestCase
 from django.core.urlresolvers import reverse
 from accounts.models import User
+from model_mommy import mommy
+from django.conf import settings
 
 
 class RegisterViewTestCase(TestCase):
@@ -22,3 +24,57 @@ class RegisterViewTestCase(TestCase):
         response = self.client.post(self.register_url, data)
         self.assertFormError(response, 'form', 'email',
                              'Este campo é obrigatório.')
+
+
+class UpdateUserTestCase(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self.url = reverse('accounts:update_user')
+        self.user = mommy.prepare(settings.AUTH_USER_MODEL)
+        self.user.set_password('123')
+        self.user.save()
+
+    def tearDown(self):
+        self.user.delete()
+
+    def test_update_user_ok(self):
+        data = {'name': 'test', 'email': 'test@test.com'}
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 302)
+        self.client.login(username=self.user.username, password='123')
+        response = self.client.post(self.url, data)
+        accounts_index_url = reverse('accounts:index')
+        self.assertRedirects(response, accounts_index_url)
+        # user = User.objects.get(username=self.user.username)
+        self.user.refresh_from_db()
+        self.assertEqual(self.user.email, 'test@test.com')
+        self.assertEqual(self.user.name, 'test')
+
+    def test_update_user_error(self):
+        data = {}
+        self.client.login(username=self.user.username, password='123')
+        response = self.client.post(self.url, data)
+        self.assertFormError(response, 'form', 'email',
+                             'Este campo é obrigatório.')
+
+
+class UpdatePasswordTestCase(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self.url = reverse('accounts:update_user')
+        self.user = mommy.prepare(settings.AUTH_USER_MODEL)
+        self.user.set_password('123')
+        self.user.save()
+
+    def tearDown(self):
+        self.user.delete()
+
+    # def test_update_password_ok(self):
+    #     data = {'old_password': '123', 'new_password1': 'teste123',
+    #             'new_password2': 'teste123'}
+    #     self.client.login(username=self.user.username, password='123')
+    #     response = self.client.post(self.url, data)
+    #     self.user.refresh_from_db()
+    #     self.assertTrue(self.user.check_password('teste123'))

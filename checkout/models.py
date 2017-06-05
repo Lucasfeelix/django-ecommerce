68d91d9d411cd1031142a83cf0cab5f0974d45cc
lucas <lucas.felix@gastecnologia.com>
2017-06-05 14:01:31 -0300

    Aula 57/58, objetivo: implantar lógica do carrinho de compras, adicionar item; Alteração nos templates category.html, product_list.html e product.html, adicionando tudo o que era comum entre estes a um outro template nomeada _product.html, criacao do modelo CarItemManager em checkout/models.py, criacao das da view CreateCarItemView e adição de url de compras em djangoecommerce/urls.py

--- checkout/models.py
+++ checkout/models.py
@@ -2,17 +2,34 @@ from django.db import models
 from catalog.models import Product
 
 
-class CarItem(models.Model):
+class CarItemManager(models.Manager):
+    def add_item(self, cart_key, product):
+        if self.filter(cart_key=cart_key, product=product).exists():
+            created = False
+            cart_item = self.get(cart_key=cart_key, product=product)
+            cart_item.quantity = cart_item.quantity + 1
+            cart_item.save()
+        else:
+            created = True
+            cart_item = CarItem.objects.create(
+                cart_key=cart_key, product=product, price=product.price
+            )
+        return cart_item, created
 
+
+class CarItem(models.Model):
     cart_key = models.CharField('Chave do Carrinho', max_length=40,
                                 db_index=True)
     product = models.ForeignKey(Product, verbose_name='Produto')
     quantity = models.PositiveIntegerField('Quantidade', default=1)
     price = models.DecimalField('Preço', decimal_places=2, max_digits=8)
 
+    objects = CarItemManager()
+
     class Meta:
         verbose_name = 'Item do Carrinho'
         verbose_name_plural = 'Itens dos Carrinhos'
+        unique_together = (('cart_key', 'product'),)
 
     def __str__(self):
         return '{} [{}]'.format(self.product, self.quantity)
--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import RedirectView
+from catalog.models import Product
+from checkout.models import CarItem
+from django.contrib import messages
 
-# Create your views here.
+
+class CreateCartItemView(RedirectView):
+
+    def get_redirect_url(self, *args, **kwargs):
+        product = get_object_or_404(Product, slug=self.kwargs['slug'])
+        if self.request.session.session_key is None:
+            self.request.session.save()
+        cart_item, created = CarItem.objects.add_item(self.request.session.session_key,
+                                                      product)
+        if created:
+            messages.success(self.request, 'Produto adicionado com sucesso!')
+        else:
+            messages.success(self.request, 'Produto atualizado com sucesso!')
+        return product.get_absolute_url()
+
+
+create_cartitem = CreateCartItemView.as_view()
